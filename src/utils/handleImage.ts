6f3552becad3071315a7639db35e01e5f54f2b67
kprzybyl <katarzynaa.przybyl@gmail.com>
2024-02-26 21:22:34 +0100

    Remove unnecessary code for handle image

--- src/utils/handleImage.ts
+++
@@ -1,9 +0,0 @@
-export const getInitialImageUri = (remoteUri?: string) => {
-    return remoteUri
-        ? { uri: remoteUri }
-        : require('../assets/default-image.png');
-};
-
-export const defaultImageOnError = () => {
-    return require('../assets/default-image.png');
-};
